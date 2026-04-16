67574ec29aed53d66386f948f425710015af7c1d
Jessica900120 <jessica900120@163.com>
2026-04-16 22:58:57 +0800

    Create tailwind.config.js

---
+++ medremind---周期性吃药提醒/tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sage: {
+          50: '#f4f9f7',
+          100: '#e8f3f0',
+          200: '#c9e4df',
+          500: '#6b9f95',
+          600: '#4a7d75',
+          900: '#1a3a33',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
